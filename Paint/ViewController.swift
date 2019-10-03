442e99ef22e8a195badd52acd2333f02e00edf47
Julio Barrera <necrocter@gmail.com>
2019-10-03 10:41:36 -0700

    Init Project

---
+++ Paint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Paint
+//
+//  Created by Julio Barrera on 9/17/19.
+//  Copyright © 2019 Julio Barrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
