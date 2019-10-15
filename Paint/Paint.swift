562b0892e2a19da96e9efae96768b7f73ca1371c
Julio Barrera <necrocter@gmail.com>
2019-10-15 10:54:52 -0700

    Add Model

---
+++ Paint/Paint.swift
@@ -0,0 +1,64 @@
+//
+//  Paint.swift
+//  Paint
+//
+//  Created by Julio Barrera on 08/10/19.
+//  Copyright © 2019 Julio Barrera. All rights reserved.
+//
+
+import UIKit.UIColor
+
+struct Paint {
+    var red: Double
+    var green: Double
+    var blue: Double
+    
+    init(red: Double, green: Double, blue: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+}
+
+extension Paint {
+    
+    private static func clamp(_ input: Double) -> Double {
+        min(max(input, 0), 1)
+    }
+    
+    mutating func brighten() {
+        red = Self.clamp(red + 0.1)
+        green = Self.clamp(green + 0.1)
+        blue = Self.clamp(blue + 0.1)
+    }
+    
+    mutating func darken() {
+        red = Self.clamp(red - 0.1)
+        green = Self.clamp(green - 0.1)
+        blue = Self.clamp(blue - 0.1)
+    }
+    
+    mutating func randomize() {
+        red = Double.random(in: 0...1)
+        green = Double.random(in: 0...1)
+        blue = Double.random(in: 0...1)
+    }
+    
+    private static func toHexString(_ input: Double, prefix: Bool = true) -> String {
+        let value = UInt8(input * 255.rounded((.down)))
+        return prefix ? String(format: "0x%02X", value) : String(format: "%02X", value)
+    }
+    
+    var redHex: String { Self.toHexString(red) }
+    var greenHex: String { Self.toHexString(green) }
+    var blueHex: String { Self.toHexString(blue) }
+    
+    var hex: String {
+        redHex + Self.toHexString(green, prefix: false) + Self.toHexString(blue, prefix: false)
+    }
+    
+    var color: UIColor {
+        return UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1)
+    }
+}
+
