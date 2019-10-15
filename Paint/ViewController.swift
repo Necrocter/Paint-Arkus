014f6d55981925c63216cab11ae2e46051e4d991
Julio Barrera <necrocter@gmail.com>
2019-10-15 10:55:38 -0700

    Add IBOutlets and IBActions

--- Paint/ViewController.swift
+++ Paint/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var colorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func onRedSliderChanged(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onGreenSliderChanged(_ sender: Any) {
+    }
+    
+    @IBAction func onBlueSliderChanged(_ sender: Any) {
+        
+    }
 
 }
 
